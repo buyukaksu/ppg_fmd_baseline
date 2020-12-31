d081ba6a04ca32c944de9d359f3b500d89dc5fbe
buyukaksu <64165297+buyukaksu@users.noreply.github.com>
2020-12-31 14:02:53 +0300

    Update

--- equalize_zoom_callback.m
+++ equalize_zoom_callback.m
@@ -1,8 +1,4 @@
-function equalize_zoom_callback (obj, evnt, g1,g2,g3,g4)
+function equalize_zoom_callback (~,~, g1,g2)
         g2.XLim = g1.XLim;
         g2.YLimMode = 'auto';
-        g3.XLim = g1.XLim;
-        g3.YLimMode = 'auto';
-        g4.XLim = g1.XLim;
-        g4.YLimMode = 'auto';
 end
